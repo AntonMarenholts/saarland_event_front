0c975148450158d6048143ca8cde79cf13b0e2d3
Anton Marenholts <ahtoh.ukr@gmail.com>
2025-08-18 18:13:15 +0200

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import { sitemap } from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({ hostname: "https://www.saarland-events-new.de/" }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/setupTests.ts", 
+    setupFiles: "./src/setupTests.ts",
   },
 });
