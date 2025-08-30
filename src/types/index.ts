d0b0153d819b92390620cccfa087d25cd5cbd62d
Anton Marenholts <ahtoh.ukr@gmail.com>
2025-08-31 00:19:57 +0200

    w

--- src/types/index.ts
+++ src/types/index.ts
@@ -25,14 +25,14 @@ export interface Event {
   city: City;
   translations: Translation[];
   status: string;
+
   // ===== ИЗМЕНЕНИЕ ЗДЕСЬ =====
-  isPremium: boolean; // Было isPremium
+  premium: boolean; // Было isPremium
   // ==========================
 
   premiumUntil?: string;
   createdByUserId: number; // Изменил на createdByUserId как в DTO
 }
-
 export interface Page<T> {
   content: T[];
   totalPages: number;
